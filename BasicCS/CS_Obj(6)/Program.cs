7a723103d1c8788328f21461f7a53420341e77b1
sputnikel0221 <111990655+sputnikel0221@users.noreply.github.com>
2022-11-10 20:46:44 +0900

    just make proj2

--- BasicCS/CS_Obj(6)/Program.cs
+++ BasicCS/CS_Obj(6)/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CS_Obj_6_
+namespace CS_Obj_5_
 {
+    //protected로 자식으로 정의된 class에서만 호출가능
+    public class Game
+    {
+        bool gameOn;
+        protected void StartGame()
+        {
+            Console.WriteLine("Start!");
+        }
+        public void StopGame()
+        {
+            Console.WriteLine("Stop!");
+        }
+    }
+
+    sealed public class LOL : Game
+    {
+        int BP;
+
+        public void gameStart()
+        {
+            StartGame();
+        }
+    }
+
+   
+
     class Program
     {
         static void Main(string[] args)
         {
+            Game mmorpg = new Game();
+            LOL league = new LOL();
+
         }
     }
 }
