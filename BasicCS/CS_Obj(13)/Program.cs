a58ce89867c3226fcbbf1930a0596c9164ad972f
sputnikel0221 <111990655+sputnikel0221@users.noreply.github.com>
2022-11-14 14:11:17 +0900

    Delegate(3)

---
+++ BasicCS/CS_Obj(13)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Obj_13_
+{
+    class Target
+    {
+        public void Do(Source obj)
+        {
+            Console.WriteLine(obj.GetResult());
+        }
+    }
+
+    class Source
+    {
+        public int GetResult()
+        {
+            return 10;
+        }
+
+        public void Test()
+        {
+            Target target = new Target();
+            target.Do(this);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
